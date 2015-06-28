f1e9613d0049de70bfb3b9627f4c3ad65d4af7a5
Daniel <danleavitt0@gmail.com>
2015-06-28 11:25:03 -0700

    cleanup index js files

--- definingFunctions/examples/sayMyName/index.js
+++
@@ -1,7 +0,0 @@
-var name = "Jeff"
-
-function sayMyName () {
-	console.log("You name is " + name)
-}
-
-document.body.innerHTML = "Open your browser console!"
--- objects/examples/dictionary/index.js
+++ objects/examples/dictionary/index.js
@@ -1,17 +1,1 @@
-// define the dictionary object
-
-var dictionary = {
-	eat: 'put (food) into the mouth and chew and swallow it.',
-	sassafras: 'an extract of the leaves or bark of the sassafras, used medicinally or in perfumery.'
-}
-
-// print out the value stored at the key "eat"
-console.log(dictionary.eat)
-
-// print out the value stored at the key "sassafras"
-var name = "sassafras"
-console.log(dictionary[name])
-
-// add an entry at the key "large" and then print out the value
-dictionary.large = 'of considerable or relatively great size, extent, or capacity.'
-console.log(dictionary.large)
+var prompt = require('sync-prompt').prompt
